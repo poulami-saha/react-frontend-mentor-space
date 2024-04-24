b0c25fb8a3e00a8db5ac690f294449ee653d8147
Poulami Saha <saha.poulami91@gmail.com>
2024-04-24 16:02:10 +0200

    Home screen modified

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,14 +1,14 @@
 const Home = () => {
   return (
     <div
-      className="flex flex-col items-center lg:flex-row align-middle text-white mx-8 lg:mx-20 lg:mt-60 justify-center align-items-center"
+      className="flex flex-col items-center lg:flex-row align-middle text-white mx-8 lg:mx-20 md:mt-60 justify-center align-items-center"
     >
-      <div className="flex flex-col items-center lg:mb-40 lg:mr-40">
-        <p className="font-barlow text-md md:text-3xl font-normal mb-2 text-[#D0D6F9]">
+      <div className="flex flex-col items-center lg:items-start lg:mb-40 lg:mr-40 ">
+        <p className="font-barlow-condensed tracking-widest text-md md:text-3xl font-normal mb-2 text-[#D0D6F9]">
           SO, YOU WANT TO TRAVEL TO
         </p>
-        <p className="font-bellefair text-6xl md:text-8xl my-6 font-normal">SPACE</p>
-        <p className="font-barlow text-wrap text-md font-normal  leading-8 text-justify text-[#D0D6F9] w-80">
+        <p className="font-bellefair text-8xl md:text-9xl md:tracking-widest my-6 font-normal">SPACE</p>
+        <p className="font-barlow text-wrap text-md font-normal  md:mx-36 lg:mx-1 lg:w-80 leading-8 text-center lg:text-justify text-[#D0D6F9]">
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
@@ -16,10 +16,10 @@ const Home = () => {
         </p>
       </div>
       <div
-        className="w-48 h-48 md:w-60 md:h-60 rounded-full flex 
- justify-center items-center bg-white mt-20 lg:mt-50 mx-20"
+        className="w-32 h-32 md:w-60 md:h-60 rounded-full flex 
+ justify-center items-center bg-white mt-20 md:mt-48 lg:mt-1 mx-20"
       >
-        <p className="z-1 font-bellefair text-[#0B0D17] text-3xl font-normal py-6">
+        <p className="z-1 font-bellefair text-[#0B0D17]  text-xl md:text-3xl font-normal py-6">
           EXPLORE
         </p>
       </div>
